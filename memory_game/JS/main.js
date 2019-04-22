10c42631527abb2964fa59bf4ef1c9ed8c943b3e
Carrie Lipper <edwardcschwartz@gmail.com>
2019-04-22 01:43:24 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -2,31 +2,35 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
 var cardsInPlay = [];
 
+function checkForMatch() {
+
+//if first and second card are equal execute confirmation alert
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+//if they are not equal execute "try again"
+} else {
+	console.log("Sorry, try again");
+}
+};
 
 
-cardsInPlay.push(0);
-console.log("User flipped queen");
-
-cardsInPlay.push(2);
-console.log("User flipped king");
-
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 
 //If length of cardsInPlay array equals 2
 if (cardsInPlay.length === 2) {
 
-//if first and second card are equal execute confirmation alert
-} if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-//if they are not equal execute "try again"
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-	alert("Sorry, try again");
-}
 
+checkForMatch();
+}
+};
+
+flipCard(0);
+flipCard(2);
 
 
 
